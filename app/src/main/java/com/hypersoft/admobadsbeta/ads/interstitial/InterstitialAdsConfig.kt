14cc929f9a4cb5c0b19c42c762d622a939cbed96
Yaqoob <social.yaqoob@gmail.com>
2024-04-23 12:28:04 +0500

    Admob Ads

--- app/src/main/java/com/hypersoft/admobadsbeta/ads/interstitial/InterstitialAdsConfig.kt
+++ app/src/main/java/com/hypersoft/admobadsbeta/ads/interstitial/InterstitialAdsConfig.kt
@@ -23,18 +23,30 @@ import com.hypersoft.admobadsbeta.di.DiComponent
  */
 
 /**
- * @param context: Can be of application class
+ * Class responsible for configuring and managing Interstitial Ads.
+ *
+ * @param context Context instance, can be Application class.
  */
 
 class InterstitialAdsConfig(private val context: Context?) : InterstitialRepository() {
 
+    // Lazy initialization of DI component
     private val diComponent by lazy { DiComponent() }
+
+    // Map to store counters for each ad type
     private val counterMap by lazy { HashMap<String, Int>() }
 
+    /**
+     * Loads an Interstitial Ad.
+     *
+     * @param adType The type of Interstitial Ad to load.
+     * @param listener Callback to handle load events.
+     */
     fun loadInterstitialAd(adType: String, listener: InterstitialOnLoadCallBack? = null) {
         var interAdId = ""
         var isRemoteEnable = false
 
+        // Determine Interstitial Ad ID and remote enable status based on ad type
         when (adType) {
             INTER_SPLASH -> {
                 interAdId = getResString(R.string.admob_inter_id)
@@ -52,6 +64,7 @@ class InterstitialAdsConfig(private val context: Context?) : InterstitialReposit
             }
         }
 
+        // Load Interstitial Ad
         loadInterstitial(
             context = context,
             adType = adType,
@@ -64,6 +77,13 @@ class InterstitialAdsConfig(private val context: Context?) : InterstitialReposit
         )
     }
 
+    /**
+     * Shows an Interstitial Ad.
+     *
+     * @param activity The activity where the ad should be displayed.
+     * @param adType The type of Interstitial Ad to show.
+     * @param listener Callback to handle show events.
+     */
     fun showInterstitialAd(activity: Activity?, adType: String, listener: InterstitialOnShowCallBack? = null) {
         showInterstitial(
             activity = activity,
@@ -74,9 +94,11 @@ class InterstitialAdsConfig(private val context: Context?) : InterstitialReposit
     }
 
     /**
-     * @param adType   Key of the Ad, it should be unique id and should be case-sensitive
-     * @param remoteCounter   Pass remote counter value, if the value is n, it will load on "n-1". In case of <= 2, it will load everytime
-     * @param loadOnStart  Determine whether ad should be load on the very first time or not?
+     * Loads an Interstitial Ad based on counter value and load conditions.
+     *
+     * @param adType Key of the Ad, it should be a unique id and case-sensitive.
+     * @param remoteCounter Pass remote counter value. If the value is n, it will load on "n-1". In case of <= 2, it will load every time.
+     * @param loadOnStart Determine whether the ad should be loaded on the very first time or not.
      *
      *  e.g. remoteCounter = 4, ad will  load on "n-1" = 3
      *      if (loadOnStart) {
@@ -104,6 +126,12 @@ class InterstitialAdsConfig(private val context: Context?) : InterstitialReposit
         }
     }
 
+    /**
+     * Retrieves string resource from the context.
+     *
+     * @param resId Resource ID of the string.
+     * @return String value of the resource.
+     */
     private fun getResString(@StringRes resId: Int): String {
         return context?.resources?.getString(resId) ?: ""
     }
